85d5eabfd10dae89aaf807019194e34a22c03f73
Luana Santini <luanasantini3@gmail.com>
2023-05-16 01:31:04 -0300

    remaking page

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function tocaSom (seletorAudio) {
+function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
     if (elemento && elemento.localName === 'audio') {
         elemento.play();
     }
     else {
-        //alert('Elemento não encontrado');
         console.log('Elemento não encontrado ou seletor inválido');
     }
 
@@ -13,27 +12,14 @@ function tocaSom (seletorAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+        trocaIcone();
     }
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
-
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-
 }
